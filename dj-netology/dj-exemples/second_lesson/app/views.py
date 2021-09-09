4e187bdfb549ff15db7a4c7dbb218c0a8077211c
tolsti <tolsti_web@mail.ru>
2021-09-10 00:00:47 +0300

    +

--- dj-netology/dj-exemples/second_lesson/app/views.py
+++ dj-netology/dj-exemples/second_lesson/app/views.py
@@ -1,8 +1,29 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.conf import settings
+from datetime import datetime
 
 
 def admin_email_view(request):
     data = settings.ADMIN_EMAIL
     return HttpResponse(f'Email - {data}')
+
+
+def info_employee_view(request):
+    data = {
+        'Ivan': 'Начальник отдела',
+        'Alex': 'Разработчик'
+    }
+
+    name = request.GET.get('name', 'не задан')
+    info = data.get(name, 'Данные не найдены')
+
+    return HttpResponse(f'Пользователь {name} - {info}')
+
+
+def info_datetime_view(request, year, month, day):
+    data = datetime(year, month, day)
+    return HttpResponse(f'Дата/время - {data}')
+
+
+def info_datetime_dtc_view(request, value):
+    return HttpResponse(f'Дата/время - {value}')
--- dj-netology/dj-exemples/second_lesson/second_lesson/settings.py
+++ dj-netology/dj-exemples/second_lesson/second_lesson/settings.py
@@ -105,3 +105,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ADMIN_EMAIL = 'admin@admin.com'
