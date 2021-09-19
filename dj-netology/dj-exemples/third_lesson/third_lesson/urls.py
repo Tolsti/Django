d8e692f6409dedaf92696d9f0f61f2652bfaba1c
tolsti <tolsti_web@mail.ru>
2021-09-19 22:38:35 +0300

    +

--- dj-netology/dj-exemples/third_lesson/third_lesson/urls.py
+++ dj-netology/dj-exemples/third_lesson/third_lesson/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-
 from players.views import list_players
 
 urlpatterns = [
     path('players/', list_players),
     path('admin/', admin.site.urls),
 ]
-
