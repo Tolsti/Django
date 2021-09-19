24a32558805090c5f257194604f49c22a95689ca
tolsti <tolsti_web@mail.ru>
2021-09-19 22:37:51 +0300

    +

--- dj-netology/dj-exemples/third_lesson/players/admin.py
+++ dj-netology/dj-exemples/third_lesson/players/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from players.models import Player, Team
+
+
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'team', 'team', 'team')
+
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Player, PlayerAdmin)
--- dj-netology/dj-exemples/third_lesson/players/views.py
+++ dj-netology/dj-exemples/third_lesson/players/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from players.models import Player
 
 
@@ -7,8 +6,25 @@ def list_players(request):
     template_name = 'players/players_list.html'
 
     players = Player.objects.all()
+    players_first_four = Player.objects.filter(id__lt=4)
+    players_after_four = Player.objects.filter(id__gt=4)
+    players_only_five = Player.objects.all()[:5]
+    players_only_five_revers = Player.objects.all().order_by('-id')[:5]
+
+    filter_name = request.GET.get('name')
+    exclude_name = request.GET.get('exclude_name')
+    players_filter_name = Player.objects.all()
+    if filter_name:
+        players_filter_name = players_filter_name.filter(name__icontains=filter_name)
+    if exclude_name:
+        players_filter_name = players_filter_name.exclude(name__icontains=exclude_name)
 
     context = {
-        'players': players
+        'players': players,
+        'players_first_four': players_first_four,
+        'players_after_four': players_after_four,
+        'players_only_five': players_only_five,
+        'players_only_five_revers': players_only_five_revers,
+        'players_filter_name': players_filter_name,
     }
     return render(request, template_name, context)
