46e16ee2b322fbd295db53a8256c5c2850be9b14
tolsti <tolsti_web@mail.ru>
2021-09-21 01:57:53 +0300

    +

--- dj-netology/dj-exemples/fourth_lesson/players/models.py
+++ dj-netology/dj-exemples/fourth_lesson/players/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Players(models.Model):
+    name = models.TextField()
+    team = models.ForeignKey('Team', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+
+class Team(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+
+class Tournament(models.Model):
+    name = models.TextField()
+    teams = models.ManyToManyField(Team)
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
