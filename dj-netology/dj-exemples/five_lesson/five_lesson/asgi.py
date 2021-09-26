152fba8918dcc1d84c750442d071aa96235a763a
tolsti <tolsti_web@mail.ru>
2021-09-26 16:42:57 +0300

    add project

---
+++ dj-netology/dj-exemples/five_lesson/five_lesson/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for five_lesson project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'five_lesson.settings')
+
+application = get_asgi_application()
