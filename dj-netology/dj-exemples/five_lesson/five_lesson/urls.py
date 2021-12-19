18a06fb093a1d096dd1f7ba10f0ba69b28060169
tolsti <tolsti_web@mail.ru>
2021-12-19 17:10:11 +0300

    +

--- dj-netology/dj-exemples/five_lesson/five_lesson/urls.py
+++ dj-netology/dj-exemples/five_lesson/five_lesson/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
+
+from app.api_view import car_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api/v1/cars/', csrf_exempt(car_view)),
+    # path('api-auth/', include('rest_framework.urls'))
 ]
